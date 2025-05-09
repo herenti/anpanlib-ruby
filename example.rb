c6d9614a13e394661ab86ec5627daa51ac9a69af
herenti <106109941+herenti@users.noreply.github.com>
2025-05-08 19:44:29 -0700

    Update example.rb

--- example.rb
+++ example.rb
@@ -1,9 +1,23 @@
 require_relative "anpanlib"
+require_relative "genrainbow" #this code can be found in the "not-mine" repository. i do not own the rainbow code.
+require_relative "translate"
 
 class Anpan < Bakery
-    def onpost(chat, message)
-        if message.length > 0
-            data = message.split(" ")
+
+    def post_await(chat, *args)
+        for i in args
+            chat.chat_post(i)
+            sleep 1
+        end
+    end
+
+    def event_onpost(chat, message)
+        content = message.content
+        if content.length > 0
+            if content.include? "youtusernamehere" #lol
+                puts "chat: #{chat.chat}: user: #{message.user} message: #{content}"
+            end
+            data = content.split(" ")
             if data.length > 1
                 func, string = data[0], data.drop(1)
             else
@@ -17,8 +31,46 @@ class Anpan < Bakery
             end
             func = func[1..-1]
             if _prefix
+                if $locked_rooms.include? chat.chat
+                    return
+                end
                 if func == "say"
                     chat.chat_post(string)
+                elsif func == "rainbow"
+                    chat.chat_post(Rainbow.new.rainbow_text(string))
+                elsif func == "tran"
+                    chat.chat_post(Translate.new.tran(string))
+                elsif func == "rsend"
+                    _chat, _message = string.split(" ", 2)
+
+                    _chat = get_chat(_chat)
+                     text = Rainbow.new.rainbow_text(_message)
+
+
+                     _postawait = []
+
+                     while text.length > 0
+                         _build = ""
+                         _text = text.split("<font")
+                         _text = _text.drop(1)
+                         for i in _text
+                            _part =  "<font#{i}"
+                            if _build.length < 2400
+                                _build += _part
+                            else
+                                break
+                            end
+                         end
+                         _postawait.append(_build)
+                         text = text.gsub(_build, "")
+                         _build = ""
+                     end
+                     Thread.new{post_await(_chat, *_postawait)}
+
+                elsif func == "msend"
+                    _chat, _message = string.split(" ", 2)
+                    get_chat(_chat).chat_post(_message)
+
                 end
             end
         end
@@ -26,5 +78,10 @@ class Anpan < Bakery
     end
 end
 
-room_list = ["dungeon"]
+
+
+room_list = []
+
+$locked_rooms = []
+
 Anpan.new("", "", room_list)
